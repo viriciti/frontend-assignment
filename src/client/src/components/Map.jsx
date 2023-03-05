0e58a310213af51263714100694877864462a7bf
gjagnoor <gjagnoor@gmail.com>
2023-03-04 20:33:06 -0500

    seclude

--- src/client/src/components/Map.jsx
+++ src/client/src/components/Map.jsx
@@ -1,28 +1,27 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
 import "./map.css";
+import env from "../.env.json";
 import LocationPin from "./LocationPin";
 
 const Map = ({ location, zoomLevel }) => {
-    console.log(location, zoomLevel)
     return (
       <div className="map">
         <h2 className="map-h2">Come Visit Us At Our Campus</h2>
 
         <div className="google-map">
-                <GoogleMapReact
-                
+          <GoogleMapReact
             bootstrapURLKeys={{
-              key: "AIzaSyA2MbR4z_w-y6XsPGRXTCQ9xSDEiWDtzms"
+              key: env.env.REACT_APP_GOOGLE_MAPS_API_KEY
             }}
             defaultCenter={location}
             defaultZoom={zoomLevel}
-        >
+          >
             <LocationPin
-          lat={location.lat}
-          lng={location.lng}
-          text={location.address}
-        />
+              lat={location.lat}
+              lng={location.lng}
+              text={location.address}
+            />
           </GoogleMapReact>
         </div>
       </div>
